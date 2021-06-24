0dd81c802c590b8386490901cf9947189331a865
KABA N'Faly <nfalykaba@gmail.com>
2021-06-24 11:36:00 -0400

    set scripts permission to x

--- scripts/killPortPID.sh
+++ scripts/killPortPID.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 PORT=$1
-kill -9 $(sudo netstat -pln | grep $PORT | awk '{print $7}' | cut -d '/' -f 1)
+kill -9 $(netstat -pln | grep $PORT | awk '{print $7}' | cut -d '/' -f 1)
 
