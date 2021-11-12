19b1d9e4650d0d80369372e4816ddadb4ff7650b
KABA N'Faly <nfalykaba@gmail.com>
2021-11-12 00:30:21 -0500

    run docker command with non-sudo users

--- install.py
+++ install.py
@@ -190,6 +190,7 @@ def install_docker():
         os.system(
             'sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose')
         os.system('sudo chmod +x /usr/local/bin/docker-compose')
+        os.system('sudo chmod 666 /var/run/docker.sock')
         os.system('sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose')
         os.system('docker-compose --version')
 
