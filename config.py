b5381f7a9b8205d55a5217208edd777414640e21
KABA N'Faly <nfalykaba@gmail.com>
2022-02-09 10:46:54 -0500

    php mbstring dg intl

--- config.py
+++ config.py
@@ -8,9 +8,7 @@ UTILS_PACKAGES = ('build-essential', 'git', 'tree', 'git-flow', 'vim', 'maven', 
 
 PHP_PACKAGES = ('apache2', 'apache2-utils', 'libapache2-mod-fcgid',
 'php-pear', 'phpVERSION', 'phpVERSION-dev', 'phpVERSION-common',
-'phpVERSION-cli', 'phpVERSION-apcu', 'phpVERSION-xml', 'phpVERSION-zip',
-'phpVERSION-curl', 'phpVERSION-fpm', 'php5.6-json', 'phpVERSION-opcache',
-'phpVERSION-readline', 'libapache2-mod-phpVERSION', 'phpVERSION-mcrypt',
+'phpVERSION-cli', 'phpVERSION-apcu', 'phpVERSION-xml', 'phpVERSION-zip', 'phpVERSION-gd', 'phpVERSION-intl', 'phpVERSION-curl', 'phpVERSION-fpm', 'php5.6-json', 'phpVERSION-opcache', 'phpVERSION-mbstring', 'phpVERSION-readline', 'libapache2-mod-phpVERSION', 'phpVERSION-mcrypt',
 'phpVERSION-mysql', 'php-mbstring', 'php-sqlite3', 'phpVERSION-xdebug', 'libpcre3-dev')
 
 NPM_MODULES = ('yo', 'gulp-cli', 'react', 'sass', 'generator-angular', 'generator-jhipster', 'yarn', 'vue@next', '@vue/cli')
