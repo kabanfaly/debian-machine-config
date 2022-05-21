1477378f77528d8b5f47cd5d65471b261c5b9737
N'Faly KABA <nfalykaba@gmail.com>
2022-05-21 14:34:05 -0400

    updating deploy-heroku script

--- scripts/deploy-heroku.sh
+++ scripts/deploy-heroku.sh
@@ -17,8 +17,8 @@ fi
 
 
 WORKSPACE=/Users/nfalykaba/Workspace
-SOURCE_DIR=$WORKSPACE/$LANGUAGE_DIR/$SOURCE_APP/
-DEST_DIR=$WORKSPACE/heroku/$HEROKU_APP/
+SOURCE_DIR=$WORKSPACE/$LANGUAGE_DIR/$SOURCE_APP
+DEST_DIR=$WORKSPACE/heroku/$HEROKU_APP
 
 if [ ! -d "$SOURCE_DIR" ]; then
   echo "$SOURCE_DIR does not exist."
@@ -29,8 +29,10 @@ if [ ! -d "$DEST_DIR" ]; then
  echo "$DEST_DIR does not exist."
  exit
 fi
-
-cd $DEST_DIR && rsync -av $SOURCE_DIR/*  $DEST_DIR/ --exclude 'dist' --exclude 'target' --exclude 'node_modules' --delete-before
+cd $DEST_DIR  
+rm -r src app content 1>/dev/null 2>&1 
+cp -r $SOURCE_DIR/*  $DEST_DIR/
+rm -rf dist target node_modules 1>/dev/null 2>&1
 git add .
 git commit -am "deploy"
 git push heroku main
