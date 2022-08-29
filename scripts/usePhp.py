ff801b7d9b9f3d4aecd8011b7c81f230a8a96a1b
N'Faly KABA <nfaly.kaba@gmail.com>
2022-08-28 21:55:32 -0400

    php installer mac

--- scripts/usePhp.py
+++ scripts/usePhp.py
@@ -6,12 +6,8 @@ PHP_VERSIONS = ['7.4', '8.0']
 
 version = sys.argv[1]
 print ('Version to use:', version)
-for v in PHP_VERSIONS:
-    if v != version:
-        print (' ------- Unlinking php@', v)
-        os.system('brew unlink php@' + v)
 
-os.system('brew link php@'+ version + ' --force')
+os.system('brew-php-switcher '+ version)
 os.system('echo > ~/.phprc')
 os.system("echo 'export PATH=" + '"/opt/homebrew/opt/php@' + version + '/bin:$PATH"' + "'>> ~/.phprc")
 os.system("echo 'export PATH=" + '"/opt/homebrew/opt/php@' + version + '/sbin:$PATH"' + "'>> ~/.phprc")
