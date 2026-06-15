6ed3f3114a49296ce7b1bde817b5f1441d8ef133
N'Faly KABA <nfaly.kaba@gmail.com>
2026-06-14 23:18:30 -0400

    Update Docker configurations and scripts: added n8n and RabbitMQ compose files, modified existing MongoDB and PostgreSQL configurations, and updated Makefile for new services. Enhanced PHP version handling in scripts.

--- scripts/usePhp.py
+++ scripts/usePhp.py
@@ -2,7 +2,7 @@
 
 import sys, os
 
-PHP_VERSIONS = ['7.4', '8.2']
+PHP_VERSIONS = ['7.4', '8.2', '8.3']
 
 version = sys.argv[1]
 print ('Version to use:', version)
@@ -11,5 +11,8 @@ os.system('brew-php-switcher '+ version)
 os.system('echo > ~/.phprc')
 os.system("echo 'export PATH=" + '"/opt/homebrew/opt/php@' + version + '/bin:$PATH"' + "'>> ~/.phprc")
 os.system("echo 'export PATH=" + '"/opt/homebrew/opt/php@' + version + '/sbin:$PATH"' + "'>> ~/.phprc")
+os.system('export LDFLAGS="-L/opt/homebrew/opt/php@' + version + '/lib"')
+os.system('export CPPFLAGS="-I/opt/homebrew/opt/php@' + version +'/include"')
+os.system('brew services restart php@' + version)
 os.system('brew services restart httpd')
 os.system('php -version')
